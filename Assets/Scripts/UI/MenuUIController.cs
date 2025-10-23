3927a5c1044f003c84b81e8926cd49dd4477b7be
Breno Vieira <brenovnc435@gmail.com>
2025-10-23 12:36:31 -0300

    Feat: Implementando o menu principal

---
+++ Assets/Scripts/UI/MenuUIController.cs
@@ -0,0 +1,20 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MenuUIController : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Floresta");
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
